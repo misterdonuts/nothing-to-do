f30beddec4cb885cae9c1948f67334f2b3ce2ab9
adshidtadka <nextage900fx@gmail.com>
2018-11-26 12:52:57 +0900

    add signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get    	'/login',   to: 'sessions#new'
   post   	'/login',   to: 'sessions#create'
   delete 	'/logout',  to: 'sessions#destroy'
+  get		'/signup',	to: 'users#new'
+  root 		'users#index'
   resources :users
-
-  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
