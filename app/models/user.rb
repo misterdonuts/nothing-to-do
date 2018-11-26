44c727a29f4f65e1c0bebf999d1c43583aa6e278
adshidtadka <nextage900fx@gmail.com>
2018-11-26 15:47:41 +0900

    add test for mail format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-	validates :user_name, presence:true
-	validates :email, presence:true
 	validates :user_name,  presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
