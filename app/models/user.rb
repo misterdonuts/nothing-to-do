b945acce909761c804224e85c1939276c06aaced
adshidtadka <nextage900fx@gmail.com>
2018-11-26 15:33:06 +0900

    add test for length and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates :user_name, presence:true
+	validates :email, presence:true
+	validates :user_name,  presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,16 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-  	@user = User.new(user_name:"example user", email:"user@example.com")
-  end
+	def setup
+		@user = User.new(user_name:"example user", email:"user@example.com")
+	end
 
-  test "should be valid" do
-  	assert @user.valid?
-  end
+	test "should be valid" do
+		assert @user.valid?
+	end
 
-  test "name should be present" do
-  	@user.user_name = "  "
-  	assert_not @user.valid?
-  end
+	test "user_name should be present" do
+		@user.user_name = ""
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email =  "  "
+		assert_not @user.valid?
+	end
+
+	test "user_name should not be too long" do
+		@user.user_name = "a" * 51
+		assert_not @user.valid?
+	end
+
+	test "email should not be too long" do
+		@user.email = "a" * 244 + "@example.com"
+		assert_not @user.valid?
+	end
 end
+	
