8bcbbdf6df0c4ddc070e2ad5bc51e63117c7e6df
adshidtadka <nextage900fx@gmail.com>
2018-11-26 14:26:38 +0900

    add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_051509) do
+ActiveRecord::Schema.define(version: 2018_11_26_052400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_051509) do
     t.integer "accept_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
