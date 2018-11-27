95c77fc5e10d3654bbf8140c31a98aea2ea6c9e4
adshidtadka <nextage900fx@gmail.com>
2018-11-27 13:41:50 +0900

    create signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
@@ -70,6 +65,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :user_name, :icon_path, :invite_num, :be_invited_num, :accept_num)
+      params.require(:user).permit(:email, :user_name, :icon_path, :invite_num, :be_invited_num, :accept_num, :password, :password_confirmation)
     end
 end
