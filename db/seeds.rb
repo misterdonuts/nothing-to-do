1dfb77ce73efde3e58e4836f76423381c2a9bc6b
adshidtadka <nextage900fx@gmail.com>
2018-11-27 11:13:16 +0900

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'aaa@gmail.com', user_name: 'sawa', icon_path: '7sadfs7s.png', invite_num: 6, be_invited_num: 100, accept_num:50)
-User.create(email: 'iiaddadadad@gmail.com', user_name: 'iida', icon_path: '9sdfsafs.png', invite_num: 0, be_invited_num: 100, accept_num:100)
-User.create(email: 'hogehoge@gmail.com', user_name: 'hoge', icon_path: '78sdfds.png', invite_num: 99, be_invited_num: 19932, accept_num:1999)
-User.create(email: 'fugafuga@gmail.com', user_name: 'fuga', icon_path: 'sa8dfsd.png', invite_num: 6, be_invited_num: 10, accept_num:50)
-User.create(email: 'piyopiyo@gmail.com', user_name: 'piyo', icon_path: 'sdfsd8f98f.png', invite_num: 100, be_invited_num: 10, accept_num:80)
+User.create(email: 'aaa@gmail.com', user_name: 'sawa', icon_path: '7sadfs7s.png', invite_num: 6, be_invited_num: 100, accept_num:50, password:'foobar', password_confirmation: 'foobar')
+User.create(email: 'iiaddadadad@gmail.com', user_name: 'iida', icon_path: '9sdfsafs.png', invite_num: 0, be_invited_num: 100, accept_num:100, password:'foobar', password_confirmation: 'foobar')
+User.create(email: 'hogehoge@gmail.com', user_name: 'hoge', icon_path: '78sdfds.png', invite_num: 99, be_invited_num: 19932, accept_num:1999, password:'foobar', password_confirmation: 'foobar')
+User.create(email: 'fugafuga@gmail.com', user_name: 'fuga', icon_path: 'sa8dfsd.png', invite_num: 6, be_invited_num: 10, accept_num:50, password:'foobar', password_confirmation: 'foobar')
+User.create(email: 'piyopiyo@gmail.com', user_name: 'piyo', icon_path: 'sdfsd8f98f.png', invite_num: 100, be_invited_num: 10, accept_num:80, password:'foobar', password_confirmation: 'foobar')
