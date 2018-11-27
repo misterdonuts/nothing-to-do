078c5adf1e123045cbcbc7d67ea061e8c6b2a3b5
adshidtadka <nextage900fx@gmail.com>
2018-11-27 14:44:17 +0900

    display flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   		# login and redirect to mypage
   	else
   		# create error message
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
   		render 'new'
   	end
   end
