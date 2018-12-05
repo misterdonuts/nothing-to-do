8cee93d9090feecd7e3f6ae2118fc09cc1554198
adshidtadka <nextage900fx@gmail.com>
2018-12-05 09:56:52 +0900

    delete user_id and grou_id from group table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_055037) do
+ActiveRecord::Schema.define(version: 2018_12_05_004913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
-    t.integer "group_id"
     t.string "group_name"
-    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "sender"
+    t.integer "reciever"
+    t.text "contents"
+    t.boolean "accept"
+    t.string "timelimit"
+    t.string "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
