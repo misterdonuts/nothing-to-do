823e7265c1ef50cb1ff702a6f49287907fba03e9
adshidtadka <nextage900fx@gmail.com>
2018-12-05 10:30:30 +0900

    create group_relations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_004913) do
+ActiveRecord::Schema.define(version: 2018_12_05_012405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "group_relations", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "group_name"
