3f008326928cd1d29ac154085e1fe57639f49b1a
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-12-24 17:25:25 +0900

    userモデルに関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
+	#カラムの名前をmount_uploaderに指定
+	mount_uploader :image, ImageUploader
 end
