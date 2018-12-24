b8e493757fbe1c5e3096d3ed5c05f50a06ecd673
adshidtadka <nextage900fx@gmail.com>
2018-12-24 15:28:48 +0900

    招待情報の表示

--- app/controllers/invitation_relations_controller.rb
+++ app/controllers/invitation_relations_controller.rb
@@ -1,4 +1,9 @@
 class InvitationRelationsController < ApplicationController
-  def show
-  end
+
+	# 招待情報の表示
+	def show
+    	@user = User.find(session[:user_id])
+    	@invitation = Invitation.find_by(id: params[:id])
+    	@invited_users = User.where(id: InvitationRelation.where(invitation_id: params[:id]).select(:user_id))
+  	end
 end
