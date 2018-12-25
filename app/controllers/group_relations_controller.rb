e986662352f073fedd0ecbd5a04a3fff4c9c89a4
adshidtadka <nextage900fx@gmail.com>
2018-12-25 17:56:04 +0900

    グループメンバーと招待候補を表示

--- app/controllers/group_relations_controller.rb
+++ app/controllers/group_relations_controller.rb
@@ -1,7 +1,11 @@
 class GroupRelationsController < ApplicationController
 	def show
-		@users = User.all
-		@group = Group.find(params[:id])
 		@group_relations = GroupRelation.where(group_id: params[:id])
+		@users_in = User.where(id: GroupRelation.where(group_id: params[:id]).select(:user_id))
+		@users_out = User.where.not(id: GroupRelation.where(group_id: params[:id]).select(:user_id))
+		@group = Group.find(params[:id])
+	end
+
+	def create
 	end
 end
