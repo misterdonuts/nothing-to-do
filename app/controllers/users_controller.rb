6029079542558ada3fc54661a6720258bdc84cf7
adshidtadka <nextage900fx@gmail.com>
2018-12-25 17:03:19 +0900

    ユーザー一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    if session[:user_id].blank?
-      redirect_to login_path
-    end
-    @login_user = User.find(session[:user_id])
-    @users = User.where(id: GroupRelation.where(group_id: GroupRelation.where(user_id: session[:user_id]).select(:group_id)).select(:user_id)).where.not(id: session[:user_id])
-    @invitation_relations = InvitationRelation.where(user_id: session[:user_id])
-    @invitations = Invitation.where(user_id: session[:user_id])
+    @users = User.all
   end
 
   # GET /users/1
