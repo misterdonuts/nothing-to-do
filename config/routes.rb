43b858ee14427fc836dae2a159c99a631bd6f4e8
adshidtadka <nextage900fx@gmail.com>
2018-12-25 16:58:15 +0900

    create home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 		'users#index'
+  root 		'home#index'
   get    	'/login',   to: 'sessions#new'
   post   	'/login',   to: 'sessions#create'
   delete 	'/logout',  to: 'sessions#destroy'
-  get		'/signup',	to: 'users#new'
+  get     '/signup',	to: 'users#new'
   post 		'/signup',  to: 'users#create'
   get 'static_pages/home'
   get 'static_pages/help'
