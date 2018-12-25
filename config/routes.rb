70df8097725b95bfac9224ceef317f76d93f9c7f
adshidtadka <nextage900fx@gmail.com>
2018-12-25 16:12:50 +0900

    グループ一覧を表示

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   resources :users
   resources :invitations
   resources :invitation_relations
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
