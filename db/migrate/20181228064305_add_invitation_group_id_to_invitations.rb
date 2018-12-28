78ac269f885bc72bdedcde72beb3531a48f6f321
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-12-28 18:21:24 +0900

    Revert "カラムが足りてなかったので追加"
    This reverts commit e4d02dbc3bf285846ed0daf937e76be6e38fc56d.

--- db/migrate/20181228064305_add_invitation_group_id_to_invitations.rb
+++
@@ -1,5 +0,0 @@
-class AddInvitationGroupIdToInvitations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :invitations, :invitation_group_id, :integer
-  end
-end
--- db/migrate/20181228065221_add_receiver_to_invitations.rb
+++
@@ -1,5 +0,0 @@
-class AddReceiverToInvitations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :invitations, :receiver, :integer
-  end
-end
