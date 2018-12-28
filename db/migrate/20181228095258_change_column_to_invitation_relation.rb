f7e2611a372e6bb11ae2df7d6f908b6845c4944f
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-12-28 19:50:39 +0900

    seed修正

--- db/migrate/20181228095258_change_column_to_invitation_relation.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToInvitationRelation < ActiveRecord::Migration[5.2]
-  def change
-  	rename_column :invitation_relations, :sender_id, :receiver_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_095258) do
+ActiveRecord::Schema.define(version: 2018_12_24_152024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2018_12_28_095258) do
 
   create_table "invitation_relations", force: :cascade do |t|
     t.integer "invitation_id"
-    t.integer "receiver_id"
+    t.integer "sender_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
