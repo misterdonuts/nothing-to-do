9c010647439aefc34fd78e47a8650d6e9ac61ab1
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-01-11 01:20:53 +0900

    jsファイル順の修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require main
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require bootstrap-sprockets
 //= require turbolinks
