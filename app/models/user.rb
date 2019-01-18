30f6b5c3bfab010f411623c13dad0311c00912f5
adshidtadka <nextage900fx@gmail.com>
2019-01-18 09:48:43 +0900

    パスワードの長さ制限を解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, on: :create
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }, on: :create
+	validates :password, presence: true, on: :create
 	#カラムの名前をmount_uploaderに指定
 	mount_uploader :image, ImageUploader, on: :update # 一字的ににバリデーションの有効タイミングを設定
 
